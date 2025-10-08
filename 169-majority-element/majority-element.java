222a54714d7b69f6fc5826978131c9b246fd2278
Prathmesh Patil <70697030+Grooot266@users.noreply.github.com>
2025-10-08 15:50:23 +0530

    Time: 1 ms (99.75%) | Memory: 53.7 MB (8.94%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+// Grooot266
+// Moore's algo
+class Solution {
+    public int majorityElement(int[] nums) {
+        int freq =0 ;
+        int ans = 0;
+        for(int i=0;i<nums.length;i++){
+            if(freq == 0){
+                ans=nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+}
